1f74a6d57b33497eb330c7a0f2c2e64cd2f89712
visowY <33413143+visowY@users.noreply.github.com>
2019-05-21 00:06:53 +0800

    修改确保英文路径

---
+++ 01_WFrameWork/Assets/WFrameWork/9.Transform重置/TransformIdendity.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+using UnityEngine.UI;
+
+namespace WFrameWork
+{
+    public class TransformIdendity
+    {
+        [MenuItem("WFrameWork/9.Transform重置")]
+        private static void MenuClicked()
+        {
+            var gameObject = new GameObject();
+            TransformLocalPositionSimplify.SetLocalPosX(gameObject.transform, 20);
+            Identity(gameObject.transform);
+        }
+
+        public static void Identity(Transform transform)
+        {
+            transform.localPosition = Vector3.zero;
+            transform.localScale = Vector3.one;
+            transform.localRotation = Quaternion.identity;
+        }
+    }
+}
