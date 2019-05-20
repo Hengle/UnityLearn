914315d2a1616b8149b5121e8303e757c8794ccd
visowY <33413143+visowy@users.noreply.github.com>
2019-05-20 17:29:00 +0800

    修改

--- 01_搭建自己的框架/Assets/WFrameWork/5.MenuItem复用/MenuItemReuse.cs
+++ 01_搭建自己的框架/Assets/WFrameWork/5.MenuItem复用/MenuItemReuse.cs
@@ -1,8 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿#if UNITY_EDITOR
+using System.IO;
+using UnityEditor;
 using UnityEngine;
+
+#endif
 
 public class MenuItemReuse 
 {
+    #if UNITY_EDITOR
+    [MenuItem("WFrameWork/5.MenuItem复用")]
+    private static void MenuItemReuseTest()
+    {
+        
+        EditorApplication.ExecuteMenuItem("WFrameWork/3.导出WFrameWork为UnityPackage");
+        Application.OpenURL("file://" + Path.Combine(Application.dataPath, "../"));
+    }
+#endif
     
 }
