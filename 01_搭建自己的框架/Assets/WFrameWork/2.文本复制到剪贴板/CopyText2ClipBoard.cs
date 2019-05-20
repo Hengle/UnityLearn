5ce7593cd309b55478898874974e5751815c6962
王悦 <yue1.wang@kaiqigu.com>
2019-05-20 17:25:20 +0800

    xiugai

---
+++ 01_搭建自己的框架/Assets/WFrameWork/2.文本复制到剪贴板/CopyText2ClipBoard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+
+namespace WFrameWork
+{
+    public class CopyText2ClipBoard
+    {
+#if UNITY_EDITOR
+        [MenuItem("WFrameWork/2.复制文本到剪贴板")]
+#endif
+        private static void MenuClicked()
+        {
+            string name = "WFrameWork" + DateTime.Now.ToString("yyyy_MMdd_HH"); //HH是24小时制 , hh是12小时制
+            GUIUtility.systemCopyBuffer = name;
+        }
+    }
+}
---
+++ 01_搭建自己的框架/Assets/WFrameWork/4.打开所在文件夹/OpenTargetFloder.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+
+namespace WFrameWork
+{
+    public class OpenTargetFolder
+    {
+        #if UNITY_EDITOR
+        [MenuItem("WFrameWork/4.打开所在的文件夹")]
+        private static void ExportPackageInFolder()
+        {
+            Application.OpenURL("file://" + Path.Combine(Application.dataPath,"../") );
+        }
+        #endif
+    }
+}
