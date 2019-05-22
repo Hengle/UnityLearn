35c9760232e7970e5992361fbbdab6cab73c45cf
visowY <33413143+visowY@users.noreply.github.com>
2019-05-22 22:46:50 +0800

    2233

---
+++ 01_WFrameWork/Assets/WFrameWork/Example/05.MonoBehaviour简化/MonoBehaviourSimplify.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WFrameWork
+{
+    public partial class MonoBehaviourSimplify : MonoBehaviour
+    {
+        public void Show()
+        {
+            GameObjectSimplify.Show(gameObject);
+        }
+
+        public void Hide()
+        {
+            GameObjectSimplify.Hide(gameObject);
+        }
+
+        public void Identity()
+        {
+            TransformSimplify.Identity(transform);
+        }
+    }
+}
