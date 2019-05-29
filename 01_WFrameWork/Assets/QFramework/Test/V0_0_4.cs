50653105d20673e8dc01d7c21f8a54a4276c6e16
Vision <wyue9881@gmail.com>
2019-05-29 22:46:12 +0800

    提交修改

--- 01_WFrameWork/Assets/QFramework/Test/V0_0_4.cs
+++ 01_WFrameWork/Assets/QFramework/Test/V0_0_4.cs
@@ -1,24 +1,24 @@
-﻿#if UNITY_EDITOR
-using NUnit.Framework;
-
-namespace QFramework
-{
-    public class V0_0_4
-    {
-        class MonoSingletonTestClass : MonoSingleton<MonoSingletonTestClass>
-        {
-
-        }
-
-        [Test]
-        public void MonoSingeltonTest()
-        {
-            var instanceA = MonoSingletonTestClass.Instance;
-            var instanceB = MonoSingletonTestClass.Instance;
-
-            Assert.AreEqual(instanceA.GetHashCode(), instanceB.GetHashCode());
-            Assert.AreEqual(instanceA.name, "MonoSingletonTestClass");
-        }
-    }
-}
-#endif
+﻿//#if UNITY_EDITOR
+//using NUnit.Framework;
+//
+//namespace QFramework
+//{
+//    public class V0_0_4
+//    {
+//        class MonoSingletonTestClass : MonoSingleton<MonoSingletonTestClass>
+//        {
+//
+//        }
+//
+//        [Test]
+//        public void MonoSingeltonTest()
+//        {
+//            var instanceA = MonoSingletonTestClass.Instance;
+//            var instanceB = MonoSingletonTestClass.Instance;
+//
+//            Assert.AreEqual(instanceA.GetHashCode(), instanceB.GetHashCode());
+//            Assert.AreEqual(instanceA.name, "MonoSingletonTestClass");
+//        }
+//    }
+//}
+//#endif
