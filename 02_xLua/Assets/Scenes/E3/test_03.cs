5bfc0d9c117123904dd93edda1a9cecef0fcc74d
王悦 <16619739143@163.com>
2019-06-20 17:30:23 +0800

    xlua学习上传

--- 02_xLua/Assets/Scenes/E3/test_03.cs
+++ 02_xLua/Assets/Scenes/E3/test_03.cs
@@ -1,22 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
 using UnityEngine;
 using XLua;
 
 public class test_03 : MonoBehaviour
 {
+    private LuaEnv _luaEnv;
 
-	private LuaEnv _luaEnv;
-	// Use this for initialization
-	void Start () {
-		_luaEnv = new LuaEnv();
-		_luaEnv.DoString("require 'my_byfile'");
-//		_luaEnv.Tick();
+    // Use this for initialization
+    void Start()
+    {
+        _luaEnv = new LuaEnv();
+        _luaEnv.AddLoader(MyLoader);
+        _luaEnv.DoString("require 'LuaScript/Main/main'");
+    }
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private byte[] MyLoader(ref string filePath)
+    {
+        string path;
+#if UNITY_EDITOR
+        path = Application.dataPath + "/" + filePath + ".lua.txt";
+#endif
+#if UNITY_ANDROID
+        path = Application.streamingAsssetsPath+"/"+filePath+".lua.txt";
+#endif
+        return File.ReadAllBytes(path);
+    }
+
+    void OnDestroy()
+    {
+        if (_luaEnv != null)
+        {
+            _luaEnv.Dispose();
+        }
+    }
+    
+    
+}
+
+public class person
+{
+    public string name;
+    public int age;
 }
