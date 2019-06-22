ce3d2ffb3dbb9600b8103556ad2b496b4aa78eed
王悦 <16619739143@163.com>
2019-06-22 09:58:39 +0800

    上传

---
+++ 04_SourceAdd/Assets/Docs/04_SourceAdd.md
@@ -0,0 +1,10 @@
+# 商业化资源加载框架
+
+## 1 程序集:
+
+## 2 Unity资源加载的方式:
+
+  >1. 拖到组件上
+  >2. Resource.Load
+  >3. AssetBundle
+  >4. AssetDataBase.LoadAtPath
---
+++ 04_SourceAdd/Assets/Editor/AB_Test.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AB_Test {
+
+	[MenuItem("Tools/打包")]
+	public static void MenuClicked1()
+	{
+		BuildPipeline.BuildAssetBundles(Application.streamingAssetsPath, BuildAssetBundleOptions.ChunkBasedCompression,
+			EditorUserBuildSettings.activeBuildTarget);
+		AssetDatabase.Refresh();
+	}
+}
