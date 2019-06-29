8088d2693d6f97185e973ee330cd9d404f70f09c
yue_viso <643493306@qq.com>
2019-06-29 23:43:13 +0800

    no message

---
+++ 02_SourceAdd/Assets/FrameWork/Editor/ABConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ABConfig" , menuName = "CreateAbConfig" , order = 0 )]
+public class ABConfig:ScriptableObject
+{
+    
+    //单个文件所在文件夹路径，会遍历文件夹下的所有prefab, 所有的prefab 的名字不能重复，保证名字的唯一性
+    public List<string> m_AllPrefab = new List<string>();
+
+    public List<FileDirAbName> m_AllFileDirAb = new List<FileDirAbName>();
+
+
+    [System.Serializable]
+    public struct FileDirAbName{
+        public string AbName;
+        public string Path;
+    }
+
+}
---
+++ 02_SourceAdd/Assets/FrameWork/Editor/ABCreate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ABCreate : MonoBehaviour
+{
+    public static string ABCONFIG_PATH = "Assets/FrameWork/Editor/ABConfig.asset"; 
+	[MenuItem("Tools/打包")]
+	public static void MenuClicked()
+	{
+		//1.吧写的ABConfig读取出来
+        ABConfig abconfig = AssetDatabase.LoadAssetAtPath<ABConfig>(ABCONFIG_PATH);
+
+        //test
+        foreach(var str in abconfig.m_AllPrefab){
+            Debug.Log(str);
+        }
+
+        foreach(var item in abconfig.m_AllFileDirAb)
+        {
+            Debug.Log(item.AbName + "   " + item.Path);
+        }
+
+	}
+}
