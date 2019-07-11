c7079e84d79c46dde71556daa603028ef0ec62ac
王悦 <16619739143@163.com>
2019-07-11 14:45:38 +0800

    动态循环列表初始设置

---
+++ 07_FancyScrollView/Assets/FancyScrollView/Examples/Sources/03_InfiniteScroll/ItemData.cs
@@ -0,0 +1,12 @@
+﻿namespace FancyScrollView.Example03
+{
+    public class ItemData
+    {
+        public string Message { get; }
+
+        public ItemData(string message)
+        {
+            Message = message;
+        }
+    }
+}
