1f89204701434d363a9d80ca91910a0d409942da
王悦 <16619739143@163.com>
2019-08-28 21:05:37 +0800

    测试提交

---
+++ 18_AbTest/Assets/Scripts/FrameWork/Editor/AssetBundleEditor/AssetBundleEditor.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace AssetBundleEditor {
+
+    //unity editor 启动时掉调用静态构造函数
+    [InitializeOnLoad]
+    public static class AssetbundleEditor
+    {
+        //%:ctrl, #:shift,&alt
+
+        const string kCreateSceneAssetBundles = "Assets/AssetBundels/CreateSceneBundle";
+
+        [MenuItem(kCreateSceneAssetBundles)]
+        public static void CreateSceneAssetBundle()
+        {
+            //清空缓存
+            Caching.ClearCache();
+
+            Object[] selObjs = Selection.objects;
+            Debug.Log(selObjs.Length);
+            for (int i = 0; i < selObjs.Length; i++)
+            {
+                Debug.Log("运行1次");
+                //固定路径(存储打完后AB包的目录)
+                string path = Application.streamingAssetsPath +"/Scenes/"+ selObjs[i].name + ".assetbundle";
+
+                string[] levels = { AssetDatabase.GetAssetPath(selObjs[i]) };
+
+                BuildTarget target = BuildTarget.NoTarget;
+#if UNITY_EDITOR
+                target = BuildTarget.StandaloneWindows64;
+#endif
+#if UNITY_ANDROID
+                target = BuildTarget.Android;
+#endif
+#if UNITY_IOS
+                target = BuildTarget.iOS;
+#endif 
+                BuildPipeline.BuildPlayer(levels, path, target, BuildOptions.BuildAdditionalStreamedScenes);
+                Debug.Log(levels[i]);
+                Debug.Log(path);
+            }
+            AssetDatabase.Refresh();
+        }
+    }
+}
---
+++ 18_AbTest/Assets/Scripts/Logic/GameStart.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameStart : MonoBehaviour
+{
+    public Button _startBtn;
+
+    void Start()
+    {
+        _startBtn.onClick.AddListener(() => { StartCoroutine(StartGame()); });
+        
+    }
+
+
+
+    //开始游戏加载打包好的场景
+    private IEnumerator StartGame()
+    {
+        Debug.Log("调用到这里");
+        string name = "/Scenes/TestScene.assetbundle";
+        string path = "file://" + Application.streamingAssetsPath + name;
+        UnityWebRequest www = UnityWebRequestAssetBundle.GetAssetBundle(path);
+        yield return www.SendWebRequest();
+
+        if (www.isHttpError || www.isNetworkError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(www);
+
+            SceneManager.LoadSceneAsync("TestScene");
+
+        }
+
+
+    }
+}
